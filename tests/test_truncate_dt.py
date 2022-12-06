2b6b9fd1b22f6cb3a31a908ab40c45394b3e8c96
João Palmeiro <joaommpalmeiro@gmail.com>
2022-12-06 09:56:24 +0000

    Closes #3

--- tests/test_truncate_dt.py
+++ tests/test_truncate_dt.py
@@ -11,4 +11,16 @@ def test_unsupported_discretization():
         ValueError,
         match="'hour' is not supported. Available discretizations: 'day', 'month', 'week'.",
     ):
-        truncate_dt(datetime(2020, 10, 16, 17, 12, 33), "hour")
+        truncate_dt(datetime(2022, 12, 6, 9, 52, 45), "hour")
+
+
+@pytest.mark.parametrize(
+    "dt,discretization,expected",
+    [
+        (datetime(2022, 12, 6, 9, 52, 45), "day", datetime(2022, 12, 6)),
+        (datetime(2022, 12, 6, 9, 52, 45), "week", datetime(2022, 12, 5)),
+        (datetime(2022, 12, 6, 9, 52, 45), "month", datetime(2022, 12, 1)),
+    ],
+)
+def test_function(dt, discretization, expected):
+    assert truncate_dt(dt, discretization) == expected
